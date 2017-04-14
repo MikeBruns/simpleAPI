2339061235c064d8aa280ba8c8499b1e40615fc7
Michael Bruns <mbruns@bgsu.edu>
2017-04-14 00:18:59 -0400

    created server, say hi to the world on port 3000

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+
+//Base url
+app.get('/', function(request, response) {
+    //Sends in JSON (thans express!)
+    response.send('Hello World!');
+});
+
+
+//Let's run it here
+app.listen(3000, function() {
+    console.log('It\'s ALIVE!');
+});
+    
