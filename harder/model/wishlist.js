41d1ee5fff8cfeccd1f31a3a7aac79e0331d83e9
Michael Bruns <michaelbruns99@gmail.com>
2017-04-22 22:44:40 -0400

    small change

--- harder/model/wishlist.js
+++ harder/model/wishlist.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
-var wishList = new Schema ({
-    title: {type: String, default: "Cool Wish List"},
-    products: [{type: ObjectId, ref:'Product'}], //creating relationship w/products object in products.js
+var wishList = new Schema({
+    title:    {type: String, default: "Cool Wish List"},
+    products: [{type: ObjectId, ref:'Product'}] //creating relationship w/products object in products.js
 });
 
 module.exports = mongoose.model('WishList', wishList);
