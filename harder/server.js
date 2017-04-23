be41a6a281fe965a82507767baa72d24baa5a4c3
Michael Bruns <michaelbruns99@gmail.com>
2017-04-22 22:55:53 -0400

    fixed error handling

--- harder/server.js
+++ harder/server.js
@@ -4,14 +4,14 @@ var express    = require('express');
 var app        = express();
 var db = mongoose.connect('mongodb://localhost/myDB');
 
-var Product = require('./model/product');
+var Product  = require('./model/product');
 var WishList = require('./model/wishlist');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
-//Creating a new product
+// Creating a new product
 app.post('/product', function(req, res){
     // One liner to replace each call as above 
     var product = new Product(req.body);
@@ -20,7 +20,7 @@ app.post('/product', function(req, res){
     
     product.save(function(err, savedProduct){
         if(err){
-            res.send(500).send({error:"Could not save product"});
+            res.status(500).send({error:"Could not save product"});
         } else {
             res.send(savedProduct);
         }
@@ -49,7 +49,7 @@ app.get('/product', function(req, res){
     // Runs asynchronously, so other things can run while querying
     Product.find({}, function(err, prods){
        if(err){
-           res.send(500).send({error:"Could fetch products"});
+           res.status(500).send({error:"Could fetch products"});
        } else {
            res.send(prods);
        }
